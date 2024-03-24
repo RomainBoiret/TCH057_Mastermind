1e1a47d5a5c5916500c1252e5985c9a34fa18f91
Martinrolo <martinrolod@hotmail.com>
2024-03-24 18:30:57 -0400

    Continué menu popup jeu

--- app/src/main/java/com/Martin_Romain_Felix/mastermind/activites/JeuActivity.java
+++ app/src/main/java/com/Martin_Romain_Felix/mastermind/activites/JeuActivity.java
@@ -1,14 +1,10 @@
 package com.Martin_Romain_Felix.mastermind.activites;
 
+import android.app.Dialog;
 import android.os.Bundle;
-import android.view.Gravity;
-import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.PopupWindow;
-import android.widget.TextView;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,8 +17,7 @@ import com.example.mastermind.R;
 public class JeuActivity extends AppCompatActivity {
 
 
-    boolean click = true;
-    PopupWindow popUp;
+    private ImageView menu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +30,103 @@ public class JeuActivity extends AppCompatActivity {
             return insets;
         });
 
+        //Chercher le bouton menu
+        menu = findViewById(R.id.menu);
+
+        //Ouvrir/fermer le menu quand on appuie dessus
+        menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ouvrirMenu();
+            }
+        });
+    }
+
+    //Fonction ouvrir menu
+    private void ouvrirMenu() {
+        //Créer le menu
+        Dialog popupMenu = new Dialog(this);
+        popupMenu.setContentView(R.layout.layout_popup_jeu);
+        popupMenu.show();
+
+        //Chercher les boutons DANS la popup
+        Button btnAbandonner = popupMenu.findViewById(R.id.btnAbandonner);
+        Button btnNouvellePartie = popupMenu.findViewById(R.id.btnNouvellePartie);
+        Button btnRetourAccueil = popupMenu.findViewById(R.id.btnRetourAccueil);
+
+
+        //Clic sur bouton nouvelle partie
+        btnNouvellePartie.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //
+                //À FAIRE
+                //
+
+
+
+
+            }
+        });
+
+        //Clic sur bouton abandonner
+        btnAbandonner.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Ouvrir autre popup pour confirmer si le joueur veut vraiment annuler
+                popupMenu.dismiss();
+
+                //Ouvrir menu abandon
+                ouvrirMenuAbandon();
+            }
+        });
+
+        //Clic sur bouton retour à l'accueil
+        btnRetourAccueil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //On ferme la popup et on finit l'activité
+                popupMenu.dismiss();
+                finish();
+            }
+        });
+
+    }
+
+
+    //Fonction qui ouvre la 2ème popup pour abandonner
+    public void ouvrirMenuAbandon() {
+        //Créer le menu popup
+        Dialog confirmerAbandon = new Dialog(this);
+        confirmerAbandon.setContentView(R.layout.layout_popup_abandonner);
+        confirmerAbandon.show();
+
+        //Chercher les boutons DANS la popup
+        Button btnAnnulerAbandon = confirmerAbandon.findViewById(R.id.btnAnnulerAbandon);
+        Button btnConfirmerAbandon = confirmerAbandon.findViewById(R.id.btnConfirmerAbandon);
+
+        //Clic sur annuler
+        btnAnnulerAbandon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //On ferme la popup et on reouvre le menu de pause
+                confirmerAbandon.dismiss();
+                ouvrirMenu();
+            }
+        });
+
+        //Clic sur abandonner
+        btnConfirmerAbandon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //
+                //À FAIRE
+                //
+
+
+
+            }
+        });
     }
 
 }
