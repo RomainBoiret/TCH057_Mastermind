23a3c6d175d2a79cef17b5a087cd80b16b890c6e
Romain <romainboiret@gmail.com>
2024-03-24 18:02:17 -0400

    feature/btnRetour_historique

--- app/src/main/java/com/Martin_Romain_Felix/mastermind/activites/HistoriqueActivity.java
+++ app/src/main/java/com/Martin_Romain_Felix/mastermind/activites/HistoriqueActivity.java
@@ -1,6 +1,10 @@
 package com.Martin_Romain_Felix.mastermind.activites;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +19,7 @@ import java.util.List;
 import com.example.mastermind.R;
 
 public class HistoriqueActivity extends AppCompatActivity {
+    private Button btnRetour;
     private RecyclerView historyRecyclerView;
     private HistoriqueAdapter historiqueAdapter;
 
@@ -22,6 +27,16 @@ public class HistoriqueActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_historique);
+
+        btnRetour = findViewById(R.id.btnRetour);
+
+        btnRetour.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
 
         historyRecyclerView = findViewById(R.id.history_recycler_view);
         historyRecyclerView.setLayoutManager(new LinearLayoutManager(this));
