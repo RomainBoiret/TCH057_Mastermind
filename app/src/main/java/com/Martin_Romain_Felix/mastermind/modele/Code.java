77614559da8765df70283b8b00000f3a99051b8e
Romain <romainboiret@gmail.com>
2024-03-25 13:59:03 -0400

    Update/modele

--- app/src/main/java/com/Martin_Romain_Felix/mastermind/modele/Code.java
+++ app/src/main/java/com/Martin_Romain_Felix/mastermind/modele/Code.java
@@ -1,4 +1,15 @@
 package com.Martin_Romain_Felix.mastermind.modele;
 
 public class Code {
+    private int[] couleurs;
+
+    public Code(int[] _couleurs)
+    {
+        this.couleurs = _couleurs;
+    }
+
+    public int[] getCouleurs()
+    {
+        return couleurs;
+    }
 }
--- app/src/main/java/com/Martin_Romain_Felix/mastermind/modele/Feedback.java
+++ app/src/main/java/com/Martin_Romain_Felix/mastermind/modele/Feedback.java
@@ -1,4 +1,22 @@
 package com.Martin_Romain_Felix.mastermind.modele;
 
 public class Feedback {
+    private int correctPosition;
+    private int correctCouleur;
+
+    public Feedback(int _correctPosition, int _correctCouleur)
+    {
+        this.correctPosition = correctPosition;
+        this.correctCouleur = _correctCouleur;
+    }
+
+    public int getCorrectPosition()
+    {
+        return correctPosition;
+    }
+
+    public int getCorrectCouleur()
+    {
+        return correctCouleur;
+    }
 }
